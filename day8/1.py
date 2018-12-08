453d93d448f5c2c7dfd7d0b274cac4951f8a10c9
dolfinsbizou <jorandon@gmail.com>
2018-12-08 21:29:40 +0100

    Day 8

---
+++ day8/1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+with open("input") as input_file:
+    input_list = input_file.readline().replace("\n", "").split(" ")
+    input_list = [int(elem) for elem in input_list]
+
+def new_node():
+    return {"children": [], "metadata": []}
+
+def process_children(input_list, output_tree):
+    """
+        Computes the children of the node output_tree, using the remaining input_list, and returns the number of elements processed in the list.
+    """
+    
+    children_count = input_list[0]
+    metadata_count = input_list[1]
+    
+    skipped_elements = 0
+
+    for i in range(children_count):
+        curr_child = new_node()
+        skipped_elements+= process_children(input_list[2+skipped_elements:], curr_child)
+        output_tree["children"].append(curr_child)
+
+    for i in range(metadata_count):
+        output_tree["metadata"].append(input_list[2+skipped_elements+i])
+
+    skipped_elements+= metadata_count+2
+
+    return skipped_elements
+
+def sum_metadatas(tree):
+    """
+        Sums up the metadatas in the tree.
+    """
+    total = sum(tree["metadata"])
+    for _, child in enumerate(tree["children"]):
+        total+= sum_metadatas(child)
+
+    return total
+
+root = new_node()
+
+process_children(input_list, root)
+
+result = sum_metadatas(root)
+
+print("Answer for puzzle #15: {}".format(result))
